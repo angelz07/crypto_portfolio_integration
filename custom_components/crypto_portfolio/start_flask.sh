40086043510c1f71e5ad259634373a8ae61056af
angelz07 <fabrice@telecom4all.be>
2024-06-21 23:43:29 +0200

    Update start_flask.sh

--- custom_components/crypto_portfolio/start_flask.sh
+++ custom_components/crypto_portfolio/start_flask.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install requests flask datetime requests-cache gunicorn
 # Script pour démarrer Gunicorn avec Flask
-exec gunicorn -w 4 -b 0.0.0.0:5000 wsgi:app
+exec gunicorn -w 4 -b 0.0.0.0:5000 wsgi:app 
